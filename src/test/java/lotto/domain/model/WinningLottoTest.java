66faf1bf77713e8d2b0a647744f58b989b3f87ac
나의찬 <cab07036@naver.com>
2025-11-01 07:55:31 +0900

    test(model): [RED] 보너스 번호 입력 성공과 중복 번호로 인한 실패 테스트 코드 작성
    당첨 번호를 입력받은 상태에서 보너스 번호를 입력 받고 검증을 거친 뒤에 당첨 로또를 최종적으로 저장할 때 중복 숫자가 없이 성공하는 테스트와
    
    중복된 숫자로 인한 실패 테스트 코드를 작성하였습니다.

---
+++ src/test/java/lotto/domain/model/WinningLottoTest.java
@@ -0,0 +1,41 @@
+package lotto.domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningLottoTest {
+
+    @DisplayName("당첨 로또에 보너스 번호를 추가할때 성공하는 테스트입니다.")
+    @Test
+    void 당첨_로또에_보너스번호_추가_성공_테스트() {
+
+        // Given
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        // When
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+
+        // Then
+        assertThat(winningLotto.getBonuseNumber()).isEqualsTo(7);
+    }
+
+    @DisplayName("당첨 로또에 보너스 번호를 추가할때 중복으로 인한 에러 테스트입니다.")
+    @Test
+    void 당첨_로또에_중복된_보너스번호_추가_실패_테스트() {
+
+        // Given
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        BonusNumber bonusNumber = new BonusNumber(6);
+
+        // When
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
