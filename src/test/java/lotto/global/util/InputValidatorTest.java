4a3b0adc5d2412b7959f07fb0bece06c5bc7c65a
나의찬 <cab07036@naver.com>
2025-11-01 02:17:00 +0900

    test(util): [RED] 입력값 검증 테스트 코드 작성
    입력값이 유효할때 True를 반환하는지, 가격이 유효하지 않을때 IllegalArgumentException를 throw하는지 검증하는 테스트 코드를 작성하였습니다.

---
+++ src/test/java/lotto/global/util/InputValidatorTest.java
@@ -0,0 +1,29 @@
+package lotto.global.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputValidatorTest {
+
+    @DisplayName("해당 가격이 유효한지 성공을 검증한다.")
+    @ParameterizedTest
+    @ValueSource(strings = "1000 2000, 5000, 10000")
+    void 가격_입력_성공_테스트(String input) {
+
+        boolean isValid =  InputValidator.validateInputPrice(input);
+        assertThat(isValid).isTrue();
+    }
+
+    @DisplayName("해당 가격이 유효한지 실패를 검증한다.")
+    @ParameterizedTest
+    @ValueSource(strings = "asdf, 0, -1000")
+    void 가격_입력_실패_테스트(String input) {
+
+        assertThatThrownBy(() -> InputValidator.validateInputPrice(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
