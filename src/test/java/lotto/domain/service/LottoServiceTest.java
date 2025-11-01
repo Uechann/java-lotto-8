1bce5efa193a4af92852316bd231db46cf2fb97c
나의찬 <cab07036@naver.com>
2025-11-02 02:38:45 +0900

    test(domain): [RED] 발행 로또들과 당첨 로또들 등수 판별 테스트 코드 작성
    당첨 로또를 통해 발행된 로또들의 통계 결과를 가져오는 테스트 코드를 작성하였습니다.

---
+++ src/test/java/lotto/domain/service/LottoServiceTest.java
@@ -0,0 +1,48 @@
+package lotto.domain.service;
+
+import jdk.swing.interop.SwingInterOpUtils;
+import lotto.domain.model.BonusNumber;
+import lotto.domain.model.Lotto;
+import lotto.domain.model.Lottos;
+import lotto.domain.model.WinningLotto;
+import lotto.domain.result.Rank;
+import lotto.domain.result.RankResult;
+import lotto.global.util.RandomUniqueNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoServiceTest {
+
+    private LottoService lottoService = new LottoService(new LottoFactory(new RandomUniqueNumberGenerator()));
+
+    @DisplayName("당첨 로또를 통해서 로또들의 당첨 등수를 판별하는 메서드 테스트")
+    @Test
+    void 로또_당첨_통계_판별_메서드_테스트() {
+        // Given
+        Lotto lotto1 = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto2 = new Lotto(List.of(1,2,3,4,5,7));
+        Lotto lotto3 = new Lotto(List.of(1,2,3,4,5,8));
+        Lottos lottos = new Lottos();
+        lottos.addLotto(lotto1);
+        lottos.addLotto(lotto2);
+        lottos.addLotto(lotto3);
+
+        Lotto lotto4 = new Lotto(List.of(1,2,3,4,5,6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningLotto winningLotto = new WinningLotto(lotto4, bonusNumber);
+
+        // When
+        RankResult rankResult = lottoService.judgeLottosRanks(lottos, winningLotto);
+
+        // Then
+        assertThat(rankResult.rankStatistics().get(Rank.FIRST)).isEqualTo(1);
+        assertThat(rankResult.rankStatistics().get(Rank.SECOND)).isEqualTo(1);
+        assertThat(rankResult.rankStatistics().get(Rank.THIRD)).isEqualTo(1);
+    }
+
+}
