1209f669f7efd69864ebe4056e32fbd68425ea6b
나의찬 <cab07036@naver.com>
2025-11-02 03:21:12 +0900

    feat(view): OutputView 기능 구현
    구입한 로또 출력과, 당첨 통계 출력에 대한 메서드를 구현하였습니다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import lotto.domain.result.RankResult;
+
+import java.util.List;
+
+public class OutputView {
+
+    private StringBuilder builder = new StringBuilder();
+
+    public OutputView() {
+    }
+
+    // 구입한 로또 출력
+    public void printPurchaseLottos(List<List<Integer>> numbers) {
+        System.out.println("\n" + numbers.size() + "개 구매했습니다.");
+
+        numbers.forEach(number -> {
+            System.out.println(number.toString());
+        });
+    }
+
+    // 당첨 통계 출력
+    public void printLottosStatistics(RankResult rankResult) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        System.out.println(rankResult.toString());
+    }
+}
