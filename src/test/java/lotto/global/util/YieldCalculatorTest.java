1c93465ab587f23a3ab372e0adacd8e5202c0b13
나의찬 <cab07036@naver.com>
2025-11-02 00:52:57 +0900

    test(util): [RED] 수익률 계산기 기능 테스트 코드 작성
    주어진 정보를 통해 계산 결과 올바른 결과가 나오는지 검증하는 테스트 코드를 작성하였습니다.

---
+++ src/test/java/lotto/global/util/YieldCalculatorTest.java
@@ -0,0 +1,23 @@
+package lotto.global.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class YieldCalculatorTest {
+
+    @DisplayName("수익률이 예상대로 계산되는지 테스트")
+    @Test
+    void 수익률_계산기_테스트() {
+        // Given
+        int purchasePrice = 8000;
+        int winningPrice = 5000;
+
+        // When
+        double yield = YieldCalculator.calculate(purchasePrice, winningPrice);
+
+        // Then
+        assertThat(yield).isEqualTo(62.5);
+    }
+}
