92f19e269b685c422453f503cf7948bfbcd2be70
나의찬 <cab07036@naver.com>
2025-11-02 03:22:18 +0900

    feat: 전체 구현한 기능들을 통합하여 구현하였습니다.
    Application에서 Contoller를 통해서 의존성들을 주입하고 Controller를 통해 도메인 로직들이 뿌려지도록 구현하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.service.LottoFactory;
+import lotto.domain.service.LottoService;
+import lotto.global.util.NumberParser;
+import lotto.global.util.RandomUniqueNumberGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(
+                new LottoService(new LottoFactory(new RandomUniqueNumberGenerator())),
+                new InputView(),
+                new OutputView(),
+                new NumberParser());
+
+        lottoController.run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,53 @@
+package lotto.controller;
+
+import lotto.domain.model.*;
+import lotto.domain.result.RankResult;
+import lotto.domain.service.LottoFactory;
+import lotto.domain.service.LottoService;
+import lotto.global.util.InputValidator;
+import lotto.global.util.NumberParser;
+import lotto.global.util.Parser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Parser<Integer> parser;
+
+    public LottoController(LottoService lottoService, InputView inputView, OutputView outputView, Parser parser) {
+        this.lottoService = lottoService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.parser = parser;
+    }
+
+    public void run() {
+        // 구입금액 입력
+        String purchasePrice = inputView.InputPurchasePrice();
+        InputValidator.validateInputPrice(purchasePrice);
+        PurchasePrice price = new PurchasePrice(Integer.parseInt(purchasePrice));
+
+        // 로또 구매
+        Lottos lottos = lottoService.purchaseLottos(price);
+
+        // 구매 로또 내역 출력 -> 값만 전달
+        outputView.printPurchaseLottos(lottos.getLottoNumbers());
+
+        // 당첨 번호 입력
+        String winningNumbers = inputView.InputWinningNumbers();
+        InputValidator.validateWinningNumbers(winningNumbers);
+
+        // 보너스 번호 입력
+        String bonusNumber = inputView.InputBonusNumber();
+        InputValidator.validateBonusNumber(bonusNumber);
+        WinningLotto winningLotto = LottoFactory.createWinningLotto(parser.parse(winningNumbers), Integer.parseInt(bonusNumber));
+
+        RankResult rankResult = lottoService.judgeLottosRanks(lottos, winningLotto);
+
+        // 당첨 통계 출력
+        outputView.printLottosStatistics(rankResult);
+    }
+}
