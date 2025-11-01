d195dafd2bc673e00d633c8882ae072eaf3d0e20
나의찬 <cab07036@naver.com>
2025-11-01 21:13:32 +0900

    test(service): [RED] 당첨 번호와 로또 생성기 테스트 코드 작성
    LottoFactory 클래스에서 당첨 번호를 생성하는 기능과, 구매 가격만큼 로또 생성이 잘되는지 테스트 하는 코드를 작성하였습니다.

---
+++ src/test/java/lotto/domain/service/LottoFactoryTest.java
@@ -0,0 +1,39 @@
+package lotto.domain.service;
+
+import lotto.domain.model.PurchasePrice;
+import lotto.domain.model.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoFactoryTest {
+
+    // 당첨 번호 생성기로 여기서 해야할까 ?
+    @Test
+    void 당첨_번호_생성기_메서드_테스트() {
+        // Given
+        List<Integer> number = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        // When
+        WinningLotto winningLotto = LottoFactory.createWinningLotto(number, bonusNumber);
+
+        // Then
+        assertThat(winningLotto.getLotto()).hasSize(6);
+        assertThat(winningLotto.getBonusNumber()).isEqualTo(bonusNumber);
+    }
+
+
+    // 입력받은 가격만큼 로또의 개수 발행해야한다.
+    @DisplayName("입력받은 가격만큼 로또의 개수를 발행하는 테스트를 구현한다.")
+    @Test
+    void 구매_가격만큼_로또를_생성하는_테스트() {
+        PurchasePrice price = new PurchasePrice(8000);
+
+        Lottos lottos = LottoFactory.createLottos(price);
+        assertThat(lottos.getLottos()).hasSize(8);
+    }
+}
