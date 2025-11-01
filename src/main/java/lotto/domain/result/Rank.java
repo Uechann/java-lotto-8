859aef5fa19f42d92684e826e00e581132f34523
나의찬 <cab07036@naver.com>
2025-11-02 00:16:43 +0900

    refactor(domain): [REFACTOR]  Rank 변수 의미 명확화
    m이 어떤 뜻을 의미하는지 모호해서 matchCount로 변경하였습니다.

--- src/main/java/lotto/domain/result/Rank.java
+++ src/main/java/lotto/domain/result/Rank.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum Rank {
-    FIRST(2000000000L, (m, b) -> m == 6),
-    SECOND(30000000L, (m, b) -> m == 5 && b),
-    THIRD(1500000L, (m, b) -> m == 5 && !b),
-    FOURTH(50000L, (m, b) -> m == 4),
-    FIFTH(5000L, (m, b) -> m == 3),
-    NONE(0L, (m, b) -> false);
+    FIRST(2000000000L, (matchCount, b) -> matchCount == 6),
+    SECOND(30000000L, (matchCount, b) -> matchCount == 5 && b),
+    THIRD(1500000L, (matchCount, b) -> matchCount == 5 && !b),
+    FOURTH(50000L, (matchCount, b) -> matchCount == 4),
+    FIFTH(5000L, (matchCount, b) -> matchCount == 3),
+    NONE(0L, (matchCount, b) -> false);
 
     private final Long prize;
     private final BiPredicate<Integer, Boolean> condition;
